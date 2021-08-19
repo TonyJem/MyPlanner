a88508e8256805997ee3f1f5359543ec1a554878
TonyJem <a.jemanov@gmail.com>
2021-08-19 17:41:52 +0300

    Add headerView

--- MyPlanner/MyPlanner/SwipingController.swift
+++ MyPlanner/MyPlanner/SwipingController.swift
@@ -2,11 +2,24 @@ import UIKit
 
 class SwipingController: UICollectionViewController {
     
+    let headerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemGray
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.backgroundColor = .green
-
+        
+        view.addSubview(headerView)
+        
+        headerView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        headerView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+        headerView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+        headerView.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
     
 }
