f2b2b12c2af14eb8a7a27fa4ee454a3d68dbfde0
TonyJem <a.jemanov@gmail.com>
2021-08-20 08:16:04 +0300

    Add HeaderView and FooterView to MainViewController

--- MyPlanner/MyPlanner/MainViewController.swift
+++ MyPlanner/MyPlanner/MainViewController.swift
@@ -2,10 +2,51 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    private let headerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .blue
+        return view
+    }()
+    
+    private let footerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .purple
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .yellow
+        
+        view.addSubview(headerView)
+        setupHeader()
+        
+        view.addSubview(footerView)
+        setupFooter()
+    }
+    
+}
+
+// MARK: - Private Methods:
+private extension MainViewController {
+    
+    func setupHeader() {
+        headerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalTo(100)
+        }
+    }
+    
+    func setupFooter() {
+        footerView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.height.equalTo(100)
+        }
     }
     
 }
