f80e48c778dd5b2310ee4c908763a120017c7c76
TonyJem <a.jemanov@gmail.com>
2021-08-20 08:46:08 +0300

    Create SwipeView as UICollectionView

---
+++ MyPlanner/MyPlanner/SwipeView.swift
@@ -0,0 +1,32 @@
+import UIKit
+import SnapKit
+
+class SwipeView: UICollectionView {
+    
+    let pages = [""]
+    
+    init() {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .horizontal
+        super.init(frame: .zero, collectionViewLayout: layout)
+        
+        backgroundColor = .cyan
+        
+        dataSource = self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+// MARK: - SwipeView DataSource
+extension SwipeView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+}
