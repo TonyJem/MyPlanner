f74c13ee5774625d3a0dab2bb044a5d5f3a7cdc6
TonyJem <a.jemanov@gmail.com>
2021-08-20 17:30:13 +0300

    Add more views to PageCell

--- MyPlanner/MyPlanner/PageCell.swift
+++ MyPlanner/MyPlanner/PageCell.swift
@@ -9,28 +9,66 @@ class PageCell: UICollectionViewCell {
         return view
     }()
     
+    private let dateView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .cyan
+        return view
+    }()
+    
+    private let calendarView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .brown
+        return view
+    }()
+    
+    private let tableView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        return view
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        addSubview(calendarContainerView)
+        
         setupLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    /*
-     make.width.equalToSuperview().multipliedBy(0.45) // 1
-     */
-    
+        
     private func setupLayout() {
+        addSubview(calendarContainerView)
         calendarContainerView.snp.makeConstraints { make in
             make.leading.equalToSuperview()
             make.top.equalToSuperview()
             make.width.equalToSuperview().multipliedBy(0.5)
             make.height.equalTo(200)
+        }
+        
+        calendarContainerView.addSubview(dateView)
+        dateView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalToSuperview()
+            make.width.equalToSuperview().multipliedBy(0.5)
+            make.bottom.equalToSuperview()
+        }
+        
+        calendarContainerView.addSubview(calendarView)
+        calendarView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.trailing.equalToSuperview()
+            make.width.equalToSuperview().multipliedBy(0.5)
+            make.bottom.equalToSuperview()
+        }
+        
+        addSubview(tableView)
+        tableView.snp.makeConstraints { make in
+            make.leading.equalToSuperview()
+            make.top.equalTo(calendarContainerView.snp.bottom).offset(10)
+            make.width.equalTo(calendarContainerView.snp.width)
+            make.bottom.equalToSuperview()
         }
     }
     
